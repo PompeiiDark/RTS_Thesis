26c5dfe044abe21cc200224d6b1edac42512e07c
PompeiiDark <55684364+PompeiiDark@users.noreply.github.com>
2023-12-01 01:25:34 +0800

    Delete RTS/Scripts/Old directory

--- RTS/Scripts/Old/Objects/Unit.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="RTS/Unit")]
-public class Unit : ScriptableObject
-{
-    public string Name;
-}
